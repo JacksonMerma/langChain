11fbb6d7ca4298b2357186d209c99d0dca056d7b
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-08-29 01:32:01 -0500

    Sending a pet color parameter and configuring the chain

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from langchain_community.chat_models.ollama import ChatOllama
 from langchain.prompts import PromptTemplate
 
-def generate_pet_name(animal_type):
-    llm = ChatOllama(model="codellama", temperature=0)
+def generate_pet_name(animal_type, pet_color):
+    llm = ChatOllama(model="codellama", temperature=0.7)
 
     # Defining prompt template
     prompt_template_name = PromptTemplate.from_template(
-        "I have a {animal_type} pet and I want a cool name for it. Suggest me five cool names for my pet."
+        "I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me five cool names for my pet."
     )
     chain = prompt_template_name | llm
-    response = chain.invoke({"animal_type": animal_type})
+    response = chain.invoke({"animal_type": animal_type, "pet_color": pet_color})
     return response
 
 if __name__ == "__main__":
-    print(generate_pet_name("cat"))
+    print(generate_pet_name("cat", "black"))
