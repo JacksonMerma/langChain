51689d8c1f2528dfd3c343ccf651556348bb98ca
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-08-29 01:27:12 -0500

    Sending a parameter (type of pet) and applying a chain

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from langchain_community.chat_models.ollama import ChatOllama
+from langchain.prompts import PromptTemplate
 
-def generate_pet_name():
+def generate_pet_name(animal_type):
     llm = ChatOllama(model="codellama", temperature=0)
-    response = llm.invoke("I have a dog pet and I want a cool name for it. Suggest me five cool names for my pet.")
-    return response.content
+
+    # Defining prompt template
+    prompt_template_name = PromptTemplate.from_template(
+        "I have a {animal_type} pet and I want a cool name for it. Suggest me five cool names for my pet."
+    )
+    chain = prompt_template_name | llm
+    response = chain.invoke({"animal_type": animal_type})
+    return response
 
 if __name__ == "__main__":
-    print(generate_pet_name())
+    print(generate_pet_name("cat"))
