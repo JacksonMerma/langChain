a8307ad5e1e10828e19019a5c13876af1bcbe437
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-08-30 01:11:03 -0500

    Implementing a function to get an answer based in a send db using the gemma model and a default base prompt

--- langchain_helper.py
+++ langchain_helper.py
@@ -4,6 +4,7 @@ from langchain_community.chat_models.ollama import ChatOllama
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.ollama.llms import Ollama
 
 embeddings = OllamaEmbeddings(model="gemma:2b")
 video_url = "https://www.youtube.com/watch?v=yXpgqAWWUrs"
@@ -16,6 +17,30 @@ def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
     docs = text_splitter.split_documents(transcript)
 
     db = FAISS.from_documents(docs, embeddings)
-    return docs
+    return db
+
+def get_response_from_query(db, query, k=4):
+    # gemma 2b can handle 2048~4096 tokens
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = "".join([d.page_content for d in docs])
+
+    llm = Ollama(model="gemma:2b")
+
+    prompt = PromptTemplate(
+            input_variables=["question", "docs"]
+            template="""
+            You are a helpful YouTube assistant that can answer questions about videos based on the video's transcript.
+            Answer the following question: {question}
+            By searching the following video transcript: {docs}
+            Only use the factual information from the transcript to answer the question.
+            If you feel like you don't have enough information to answer the question, say 'I don't know'
+            Your answers should be detailed.
+            """
+            )
+    chain = prompt | llm
+    response = chain.run(question=query, docs=docs_page_content)
+    response = response.replace("\n", "")
+    return response
+            
 
 print(create_vector_db_from_youtube_url(video_url))
