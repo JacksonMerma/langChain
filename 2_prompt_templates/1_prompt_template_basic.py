10ede362d14aa14eb3a777bb0f28a510a737a54b
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-08-31 09:56:25 -0500

    Creating template with multiple inputs

--- 2_prompt_templates/1_prompt_template_basic.py
+++ 2_prompt_templates/1_prompt_template_basic.py
@@ -6,4 +6,10 @@ from langchain_core.messages import HumanMessage
 template = "Tell me a joke about {topic}."
 prompt_template = ChatPromptTemplate.from_template(template)
 prompt = prompt_template.invoke({"topic": "cats"})
-print(prompt)
+
+# Template with multiple inputs
+template_multiple = """ You are a helpful assistant.
+Human: Tell me a {adjetive} story about a {animal}.
+Assistant:"""
+prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+prompt = prompt_multiple.invoke({"adjetive": "funny", "animal": "dog"})
